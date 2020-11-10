7707e329c111cf44ddd072d96f21f671bac53d28
JudasPriest18 <73815035+JudasPriest18@users.noreply.github.com>
2020-11-10 18:39:10 +1000

    add plugins

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,23 @@
 const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 
 module.exports = {
     context: path.resolve(__dirname, 'src'),
     entry: {
-        maim: './index.js',
+        main: './index.js',
         analytics: './analytics.js',
     },
     output: {
         filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'dist')
-    }
-}     
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './index.html',
+        }),
+        new CleanWebpackPlugin(),
+        
+    ]
+
+}
